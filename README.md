c0088274d0a26176b3479e829f1f0afa7836c458
Autarc <autarc@gmail.com>
2015-10-15 02:15:15 +0200

    update setup

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-Sonarvio
-========
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
 
-Extension to identify music in videos
+Sonarvio Extension
+==================
+
+A browser extension to identify music in videos at websites. It supports MediaSource Extension (MSE)
+streams and static video files (WebM/MP4). Currently its still work in progress and not released at one of the browser extension markets. For now a limit of 100MB is set for the provided source data.
+
+
+## Builds
+
+- [Chrome Extension v0.5](http://sonarvio.com/__/sonarvio.crx)
+- [Firefox Extension v0.5](http://sonarvio.com/__/sonarvio.xpi)
--- build.js
+++ build.js
@@ -5,206 +5,308 @@
  *
  */
 
-const path = require('path')
-const spawn = require('child_process').spawn
+var path = require('path')
+// var spawn = require('child_process').spawn
 
-const fs = require('fs-extra')
-const chokidar = require('chokidar')
-const webpack = require('webpack')
+var fs = require('fs-extra')
+var chokidar = require('chokidar')
+var webpack = require('webpack')
+var glob = require('glob')
+var JSZip = require('jszip')
+var ChromeExtension = require('crx')
 
-const nib = require('nib')
-const merge = require('deep-merge')(function (target, source) {
+var merge = require('deep-merge')(function (target, source) {
   if (target instanceof Array) {
     return [].concat(target, source)
   }
   return source
 })
 
-const manifest = require('./package.json')
+var autoprefixer = require('autoprefixer')
+var pxtorem = require('postcss-pxtorem')
+var lost = require('lost')
+var rucksack = require('rucksack-css')
 
-// TODO:
-// - parse argument ! -> else use default
-//
-// environment mode: development (default) - release
-const isProduction = (process.env.NODE_ENV === 'production') || process.argv.length > 2
+var manifest = require('./package.json')
 
+// environment (default mode: development)
+var env = {
+  SHARED_DIRECTORY: path.resolve(__dirname, 'shared'),
+  ASSETS_DIRECTORY: path.resolve(__dirname, 'shared/assets'),
+  PLATFORM_DIRECTORY: path.resolve(__dirname, 'platform'),
+  ENTRY_FILE: path.resolve(__dirname, 'shared/code/main.js'),
+  DIST_DIRECTORY: path.resolve(__dirname, path.dirname(manifest.main).split('/')[0]),
+  DIST_DIRECTORY_REFERENCE: path.resolve(__dirname, path.dirname(manifest.main)), // == bookmarklet
+  DIST_FILENAME: path.basename(manifest.main, path.extname(manifest.main)),
+  isProduction: (process.env.NODE_ENV === 'production') || process.argv.length > 2
+}
 
-const inputFile = __dirname + '/shared/code/main.js'
+if (env.isProduction) {
+  fs.removeSync(env.DIST_DIRECTORY)
+}
 
-const distDir = __dirname + '/' + manifest.main.split('/')[0] + '/' // dist
-const exportFile = path.basename(manifest.main)
-const exportDir = manifest.main.replace(exportFile, '')
-const exportName = path.basename(manifest.main, path.extname(manifest.main)) // sonarvio
+// #1 - copy assets into bookmarklet (e.g. '/icons/...')
+if (!env.isProduction) {
+  chokidar.watch(env.ASSETS_DIRECTORY).on('all', function (e, path) {
+    if (['change', 'add'].indexOf(e) > -1) {
+      fs.copySync(env.ASSETS_DIRECTORY, env.DIST_DIRECTORY_REFERENCE)
+    }
+  })
+} else {
+  fs.copySync(env.ASSETS_DIRECTORY, env.DIST_DIRECTORY_REFERENCE)
+}
 
-const platformDir = __dirname + '/platform/'
-const sharedDir = __dirname + '/shared/'
-
-
-// Default - generic settings
+// #2 - bundle shared code (~ content script) into bookmarklet
 var config = {
-  devtool: 'sourcemap',
-  debug: true,
-  entry: inputFile,
+  target: 'web',
+  entry: env.ENTRY_FILE,
   output: {
-    path: exportDir,
-    filename: exportFile
-    // library: exportName,
-    // libraryTarget: 'umd' // wrapping ...
+    path: env.DIST_DIRECTORY_REFERENCE,
+    filename: env.DIST_FILENAME + '.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [
-    // new webpack.DefinePlugin({
-    //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    // })
-  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        include: env.SHARED_DIRECTORY,
         loader: 'babel',
         query: {
-          optional: [
-            'runtime',
-            // http://babeljs.io/blog/2015/03/31/5.0.0/#stage-0:-class-properties
-            // https://gist.github.com/jeffmo/054df782c05639da2adb
-            // 'es7.classProperties',
-            // http://babeljs.io/blog/2015/05/14/function-bind/
-            // https://github.com/zenparsing/es-function-bind
-            // 'es7.functionBind'
-          ],
+          optional: ['runtime'],
           stage: 0
         }
       },
       {
-        test: /\.styl$/,
-        exclude: /node_modules/,
-        // https://medium.com/seek-ui-engineering/the-end-of-global-css-90d2a4a06284
-        loader: !isProduction
-                ?'style-loader!css-loader?modules&importLoaders=1&localIdentName=[path][name]__[local]--[hash:base64:5]!stylus-loader'
-                :'style-loader!css-loader?modules!stylus-loader'
-        // query: {}
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        include: env.SHARED_DIRECTORY,
+        loader: 'url',
+        query: {
+          limit: 10240
+        }
       }
-      // {
-      //   test: /\.(jpe?g|png|gif|svg)$/i,
-      //   loaders: [
-      //     'file?hash=sha512&digest=hex&name=[hash].[ext]',
-      //     // 'url?limit=100000', // hash, inlining ?
-      //     'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      //   ]
-      // },
-      // { // e.g. referenced in 'mime-db' (dependency of request)
-      //   test: /\.json$/,
-      //   exclude: '/node_modules/',
-      //   loader: 'json-loader'
-      //   // query: {}
-      // }
     ]
   },
   stylus: {
-    // perhaps instead of nib - just autoprefixer or postcss:
-    // https://github.com/postcss/postcss#built-with-postcss
-    use: [nib()],
-    import: ['nib'],
     errors: true
+  },
+  postcss: function(){
+    return [
+      lost({/** https://github.com/corysimmons/lost#global-grid-settings **/
+        flexbox: 'flex'
+      }),
+      rucksack({/** https://github.com/simplaio/rucksack#options **/}),
+      pxtorem({/** https://github.com/cuth/postcss-pxtorem#options **/}),
+      autoprefixer({/** https://github.com/postcss/autoprefixer-core#usage **/
+        // browsers: ['last 2 versions']
+      })
+    ]
   }
 }
 
-if (isProduction) {
+// development: build + watch
+if (!env.isProduction) {
   config = merge(config, {
     devtool: 'eval',
-    debug: false,
+    module: {
+      loaders: [
+        {
+          test: /\.styl$/,
+          include: env.SHARED_DIRECTORY,
+          // https://medium.com/seek-ui-engineering/the-end-of-global-css-90d2a4a06284
+          loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[path][name]__[local]--[hash:base64:5]!stylus-loader'
+        }
+      ]
+    }
+  })
+  var ready = false
+  webpack(config).watch(100, function (error, stats) {
+    if (ready) {
+      if (error) {
+        return console.error('error', error)
+      }
+      return console.log(new Date().toISOString(), ' - [sonarvio-extension]', stats.toString())
+    }
+    ready = true
+    notify.apply(null, arguments)
+  })
+} else {
+  // production: release
+  config = merge(config, {
+    devtool: 'source-map',
     plugins: [
       new webpack.PrefetchPlugin('react'),
-      // new webpack.PrefetchPlugin('react/lib/ReactComponentBrowserEnvironment')
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({ sourceMap: false })
+      new webpack.optimize.UglifyJsPlugin({
+        // sourceMap: false,
+        compress: {
+          warnings: false
+        }
+      })
     ],
+    module: {
+      loaders: [
+        {
+          test: /\.styl$/,
+          include: env.SHARED_DIRECTORY,
+          // https://medium.com/seek-ui-engineering/the-end-of-global-css-90d2a4a06284
+          loader: 'style-loader!css-loader?modules!stylus-loader'
+        }
+      ]
+    },
     stylus: {
       compress: true
     }
   })
+  webpack(config).run(notify)
 }
 
-
-var platforms = []
-
-var assetsDir = sharedDir + 'assets/'
-
-// iterate over all platforms + copy setup files (e.g. all assets) | build + watch
-fs.readdirSync(platformDir).forEach(function (platform) {
-  var srcPlatformDir = platformDir + platform + '/'
-  var distPlatformDir = distDir + platform + '/'
-
-  platforms.push(distPlatformDir)
-
-  if (isProduction) {
-    fs.copySync(assetsDir, distPlatformDir)
-    fs.copySync(srcPlatformDir, distPlatformDir)
-  } else { // development - specific watcher
-    chokidar.watch(srcPlatformDir).on('all', function (e, path) {
-      if (['change', 'add'].indexOf(e) > -1) {
-        fs.copySync(path, path.replace(srcPlatformDir, distPlatformDir))
-      }
-    })
+// async ready
+function notify (error, stats) {
+  if (error) {
+    return console.error('error', error)
   }
-})
+  console.log(new Date().toISOString(), ' - [sonarvio-extension]', stats.toString())
 
-// development - shared watcher
-if (!isProduction) {
-  chokidar.watch(assetsDir).on('all', function (e, path) {
-    if (['change', 'add'].indexOf(e) > -1) {
-      platforms.forEach(function (distPlatformDir) {
-        fs.copySync(path, path.replace(assetsDir, distPlatformDir))
+  // #3 - include all the bookmarklet files and transfer them into the specific platform packages
+  // (incl. now the reference file-loader hashed data)
+  fs.readdirSync(env.PLATFORM_DIRECTORY).forEach(function (platform) {
+    if (env.DIST_DIRECTORY_REFERENCE.indexOf(platform) === -1) {
+      // if (platform  === 'firefox') {
+      //   platform += '/data'
+      // }
+      var distPlatformDirectory = path.join(env.DIST_DIRECTORY, platform)
+      if (!env.isProduction) {
+        chokidar.watch(env.DIST_DIRECTORY_REFERENCE).on('all', function (e, path) {
+          if (['change', 'add'].indexOf(e) > -1) {
+            fs.copySync(path, path.replace(env.DIST_DIRECTORY_REFERENCE, distPlatformDirectory))
+          }
+        })
+      } else {
+        fs.copySync(env.DIST_DIRECTORY_REFERENCE, distPlatformDirectory)
+      }
+    }
+  })
+
+  // #4 - check all platforms for '.webextensions.js' files and sync them between the specified platforms
+  // + #5 - copy all files from the platforms directories into the dist platforms folders !
+  // TODO:
+  // - include regular handler for IE/Edge and safari as well
+  var webextensionsVendors = ['chrome', 'opera', 'firefox']
+
+  webextensionsVendors.forEach(function (vendor) {
+    var vendorSourceDirectory = path.join(env.PLATFORM_DIRECTORY, vendor)
+    var vendorDistDirectory = path.join(env.DIST_DIRECTORY, vendor)
+    var others = webextensionsVendors.reduce(function (others, otherVendor) {
+      if (otherVendor !== vendor) {
+        others.push(path.join(env.PLATFORM_DIRECTORY, otherVendor))
+      }
+      return others
+    }, [])
+    if (!env.isProduction) {
+      chokidar.watch(vendorSourceDirectory).on('all', function (e, path) {
+        if (['change', 'add'].indexOf(e) > -1) {
+          if ((/\.webextensions\.js$/).test(path)) {
+            others.forEach(function (othersDirectory) { // internaly prevents - as it detect the same
+              fs.copySync(path, path.replace(vendorSourceDirectory, othersDirectory))
+            })
+          }
+          fs.copySync(path, path.replace(vendorSourceDirectory, vendorDistDirectory))
+        }
+      })
+    } else { // copy static files in sync
+      fs.readdirSync(vendorSourceDirectory).forEach(function (file) {
+        var vendorFile = path.join(vendorSourceDirectory, file)
+        if ((/\.webextensions\.js$/).test(file)) {
+          others.forEach(function (othersDirectory) {
+            fs.copySync(vendorFile, path.join(othersDirectory, file))
+          })
+        }
+        fs.copySync(vendorFile, path.join(vendorDistDirectory, file))
       })
     }
   })
-}
-
-// exclude origin reference
-var references = platforms.filter(function (platform) {
-  return platform.indexOf(config.output.path) === -1
-})
-
-// spawn process for updating the firefox extension build + release package
-var jpmCLI = null
-
-if (isProduction) {
-  webpack(config).run(copyAndLog)
-  jpmCLI = spawn('jpm', ['xpi', '-v'], {
-    cwd: distDir + 'firefox/'
-  })
-} else {
-  webpack(config).watch(100, copyAndLog)
-  jpmCLI = spawn('jpm', ['watchpost', '--post-url', 'http://localhost:8888/', '-v'], {
-    cwd: distDir + 'firefox/'
-  })
-}
-jpmCLI.stdout.pipe(process.stdout)
-jpmCLI.stderr.pipe(process.stderr)
-jpmCLI.on('error', console.error.bind(console))
 
 
-/**
- * Creates copies based on the reference of the 'sonarvio.js' script.
- *
- * @param  {[type]} err   [description]
- * @param  {[type]} stats [description]
- * @return {[type]}       [description]
- */
-function copyAndLog (err, stats) {
-  if (err) {
-    return console.error(err)
+  // #6 - build package
+
+  // wrap chrome
+  var chromeDirectory = path.join(env.DIST_DIRECTORY, 'chrome')
+  var chromeRelease = path.join(chromeDirectory, 'release')
+  var chromeExtension = path.join(chromeRelease, 'sonarvio.crx')
+  fs.ensureDirSync(chromeRelease)
+
+  if (env.isProduction) {
+    var crx = new ChromeExtension({
+      // codebase: '' // online reference for auto-updates
+      privateKey: fs.readFileSync(path.join(__dirname, 'chrome-key.pem')),
+      rootDirectory: chromeDirectory
+    })
+    crx.load(chromeDirectory).then(function(){
+      return crx.loadContents()
+    })
+    .then(function (archiveBuffer) {
+      return crx.pack(archiveBuffer)
+    })
+    .then(function (crxBuffer) {
+      fs.writeFileSync(chromeExtension, crxBuffer)
+    })
+    .catch(console.error.bind(console))
   }
-  references.forEach(function (platform) {
-    // custom rule to specify firefox 'data/' directory
-    if (platform.indexOf('firefox') > -1) {
-      platform += '/data'
+
+  // wrap firefox
+  // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Chrome_incompatibilities
+  // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Packaging_and_installation
+  // https://wiki.mozilla.org/WebExtensions#Packaging_2
+  var firefoxDirectory = path.join(env.DIST_DIRECTORY, 'firefox')
+  var firefoxRelease = path.join(firefoxDirectory, 'release')
+  var firefoxExtension = path.join(firefoxRelease, 'sonarvio.xpi')
+  fs.ensureDirSync(firefoxRelease)
+
+  var zip = new JSZip()
+  glob.sync('**/**', { cwd: firefoxDirectory, mark: true }).forEach(function (file) {
+    if (file.charAt(file.length-1) !== '/' && file.indexOf(firefoxRelease) === -1) {
+      zip.file(file, fs.readFileSync(path.join(firefoxDirectory, file)))
     }
-    fs.copySync(config.output.path + config.output.filename, platform + '/' + config.output.filename)
   })
-  console.log('[BUILD] - ', stats.toString())
+  fs.writeFileSync(firefoxExtension, zip.generate({ // content
+    type: 'nodebuffer',
+    platform: process.platform
+  }))
+
+  // if (!env.isProduction) {
+  //   chokidar.watch(firefoxDirectory).on('all', function (e, path) {
+  //     if (['change', 'add'].indexOf(e) > -1 && path.indexOf(firefoxRelease) === -1) {
+  //       zip.file(path.replace(firefoxDirectory, ''), fs.readFileSync(path))
+  //       fs.writeFileSync(firefoxExtension, zip.generate({ // content
+  //         type: 'nodebuffer',
+  //         platform: process.platform
+  //       }))
+  //     }
+  //   })
+  // }
+
+  // --- currently jpm and the WebExtensions API doesn't worker together  ---
+  // http://stackoverflow.com/questions/32670794/how-do-i-use-jpm-with-webextension-in-firefox
+  //
+  // spawn process for updating the firefox extension build + release package
+  // var jpmCLI = null
+  // if (!isProduction) {
+  //   jpmCLI = spawn('jpm', ['watchpost', '--post-url', 'http://localhost:8888/', '-v'], {
+  //     cwd: path.join(env.DIST_DIRECTORY, 'firefox/')
+  //   })
+  // } else {
+  //   jpmCLI = spawn('jpm', ['xpi', '-v'], {
+  //     cwd: path.join(env.DIST_DIRECTORY, 'firefox/')
+  //   })
+  // }
+  // jpmCLI.stdout.pipe(process.stdout)
+  // jpmCLI.stderr.pipe(process.stderr)
+  // jpmCLI.on('error', console.error.bind(console))
+
+  if (env.isProduction) {
+    console.log('[DONE]');
+  }
 }
